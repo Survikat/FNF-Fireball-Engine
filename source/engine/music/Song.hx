f3a16c709a3dee57a09bb90182654442d35a89a3
SILVERSTAR <silverstar.apollo@gmail.com>
2026-08-07 17:19:41 +0800

    correctness

--- source/engine/music/Song.hx
+++ source/engine/music/Song.hx
@@ -3,41 +3,67 @@ package engine.music;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.sound.FlxSound;
-import flixel.system.FlxAssets.FlxSoundAsset;
 import flixel.util.FlxSignal.FlxTypedSignal;
 
 class TimeSignature
 {
-    public var numerator:Int;
-    public var denominator:Int;
+    public var numerator(get, set):Int;
+    public var denominator(get, set):Int;
 
-    public function new(num:Int = 4, denom:Int = 4)
+    private var _numerator:Int;
+    private var _denominator:Int;
+
+    public function new(a:Int, b:Int)
     {
-        numerator = num;
-        denominator = denom;
+        numerator = a;
+        denominator = b;
     }
-}
 
-typedef SongDesc = {
-    var instrumentalAsset:FlxSoundAsset;
-    var bpm:Float;
-    var timeSignature:TimeSignature;
+    public function get_numerator():Int
+    {
+        return _numerator;
+    }
+
+    public function set_numerator(value:Int):Int
+    {
+        _numerator = Std.int(Math.max(value, 1));
+        return _numerator;
+    }
+
+    public function get_denominator():Int
+    {
+        return _denominator;
+    }
+
+    public function set_denominator(value:Int):Int
+    {
+        _denominator = Std.int(Math.max(value, 1));
+        return _denominator;
+    }
 }
 
 class Song extends FlxBasic
 {
-    public var instrumental = new FlxSound();
-    public var onBeat = new FlxTypedSignal<Int -> Void>();
-    public var onQuarter = new FlxTypedSignal<Int -> Void>();
-    public var onStep = new FlxTypedSignal<Int -> Void>();
+    public var instrumental:FlxSound;
+
+    public var onMeasure:FlxTypedSignal<Int->Void>;
+    public var onBeat:FlxTypedSignal<Int->Void>;
+    public var onQuarter:FlxTypedSignal<Int->Void>;
+    public var onStep:FlxTypedSignal<Int->Void>;
     
+    /**
+     * Time in seconds
+     */
     public var time(get, set):Float;
+    
     public var bpm(get, set):Float;
     public var timeSignature(get, set):TimeSignature;
 
+    public var measureDuration(get, never):Float;
     public var beatDuration(get, never):Float;
     public var quarterDuration(get, never):Float;
     public var stepDuration(get, never):Float;
+    public var curMeasure(get, never):Float;
     public var curBeat(get, never):Float;
     public var curQuarter(get, never):Float;
     public var curStep(get, never):Float;
@@ -47,49 +73,64 @@ class Song extends FlxBasic
     private var _beatDuration:Float;
     private var _quarterDuration:Float;
     private var _stepDuration:Float;
-    private var _curBeat:Int = 0;
-    private var _curQuarter:Int = 0;
-    private var _curStep:Int = 0;
-    private var _lastBeat:Int = -1;
-    private var _lastQuarter:Int = -1;
-    private var _lastStep:Int = -1;
+    private var _curMeasure:Int;
+    private var _curBeat:Int;
+    private var _curQuarter:Int;
+    private var _curStep:Int;
+    private var _lastMeasure:Int;
+    private var _lastBeat:Int;
+    private var _lastQuarter:Int;
+    private var _lastStep:Int;
     
     // todo: support whats below
     // public var opponentVoice:Null<FlxSound>;
     // public var playerVoice:Null<FlxSound>;
     // public var combinedVoice:Null<FlxSound>;
 
-    public function new(desc:SongDesc)
+    public function new()
     {
         super();
-
-        if (desc == null)
-        {
-            FlxG.log.error("Expected descriptor, got null");
-        }
-
-        if (desc.instrumentalAsset == null)
-        {
-            FlxG.log.error("Expected instrumental asset, got null");
-        }
-
-        if (desc.timeSignature == null)
-        {
-            FlxG.log.error("Expected time signature, got null");
-        }
-
-        instrumental.load(desc.instrumentalAsset);
+        
+        instrumental = new FlxSound();
         FlxG.sound.list.add(instrumental);
+        // add voices to the list too
 
-        _bpm = desc.bpm;
-        _timeSignature = desc.timeSignature;
-        _updateDurations();
+        onMeasure = new FlxTypedSignal<Int->Void>();
+        onBeat = new FlxTypedSignal<Int->Void>();
+        onQuarter = new FlxTypedSignal<Int->Void>();
+        onStep = new FlxTypedSignal<Int->Void>();
+
+        _bpm = 0;
+        _timeSignature = new TimeSignature(1, 1);
+        
+        _beatDuration = 0;
+        _quarterDuration = 0;
+        _stepDuration = 0;
+        
+        _curMeasure = 0;
+        _curBeat = 0;
+        _curQuarter = 0;
+        _curStep = 0;
+
+        _lastMeasure = -1;
+        _lastBeat = -1;
+        _lastQuarter = -1;
+        _lastStep = -1;
     }
 
     override public function destroy():Void
     {
+        onMeasure.removeAll();
+        onMeasure = null;
+        
         onBeat.removeAll();
         onBeat = null;
+
+        onQuarter.removeAll();
+        onQuarter = null;
+
+        onStep.removeAll();
+        onStep = null;
 
         instrumental.destroy();
         instrumental = null;
@@ -101,16 +142,22 @@ class Song extends FlxBasic
     {
         super.update(elapsed);
 
-        final songTime = time;
-
-        _curBeat = Std.int(songTime / _beatDuration);
-        _curQuarter = Std.int(songTime / _quarterDuration);
-        _curStep = Std.int(songTime / _stepDuration);
-
-        if (_curBeat != _lastBeat)
+        if (_bpm == 0)
         {
-            _lastBeat = _curBeat;
-            onBeat.dispatch(_curBeat);
+            return;
+        }
+
+        final curTime:Float = time;
+
+        _curBeat = Std.int(curTime / _beatDuration);
+        _curMeasure = Std.int(_curBeat / _timeSignature.numerator);
+        _curQuarter = Std.int(curTime / _quarterDuration);
+        _curStep = Std.int(curTime / _stepDuration);
+
+        if (_curStep != _lastStep)
+        {
+            _lastStep = _curStep;
+            onStep.dispatch(_curStep);
         }
 
         if (_curQuarter != _lastQuarter)
@@ -119,21 +166,36 @@ class Song extends FlxBasic
             onQuarter.dispatch(_curQuarter);
         }
 
-        if (_curStep != _lastStep)
+        if (_curBeat != _lastBeat)
         {
-            _lastStep = _curStep;
-            onStep.dispatch(_curStep);
+            _lastBeat = _curBeat;
+            onBeat.dispatch(_curBeat);
+        }
+
+        if (_curMeasure != _lastMeasure)
+        {
+            _lastMeasure = _curMeasure;
+            onMeasure.dispatch(_curMeasure);
         }
     }
 
-    // todo: expand api
+    // todo: expand api (add more functions)
     public function play():Void
     {
         instrumental.play();
+        // play voices
     }
 
     private function _updateDurations():Void
     {
+        if (_bpm == 0)
+        {
+            _beatDuration = 0;
+            _quarterDuration = 0;
+            _stepDuration = 0;
+            return;
+        }
+
         _beatDuration = 60 / _bpm;
         _quarterDuration = _beatDuration * (_timeSignature.denominator / 4);
         _stepDuration = _quarterDuration / 4;
@@ -144,9 +206,9 @@ class Song extends FlxBasic
         return instrumental.time / 1000;
     }
 
-    public function set_time(newTime:Float):Float
+    public function set_time(value:Float):Float
     {
-        instrumental.time = newTime * 1000;
+        instrumental.time = value * 1000;
         return instrumental.time;
     }
 
@@ -157,7 +219,7 @@ class Song extends FlxBasic
 
     public function set_bpm(value:Float):Float
     {
-        _bpm = value;
+        _bpm = Math.max(value, 0);
         _updateDurations();
         return _bpm;
     }
@@ -179,6 +241,11 @@ class Song extends FlxBasic
         return _beatDuration;
     }
 
+    public function get_measureDuration():Float
+    {
+        return (_bpm > 0) ? _timeSignature.numerator * _beatDuration : 0;
+    }
+
     public function get_quarterDuration():Float
     {
         return _quarterDuration;
@@ -187,6 +254,11 @@ class Song extends FlxBasic
     public function get_stepDuration():Float
     {
         return _stepDuration;
+    }
+
+    public function get_curMeasure():Float
+    {
+        return _curMeasure;
     }
 
     public function get_curBeat():Float
--- source/game/TestState.hx
+++ source/game/TestState.hx
@@ -1,36 +1,40 @@
 package game;
 
+import engine.music.Song.TimeSignature;
 import engine.states.MusicalState;
 import flixel.text.FlxText;
 
 class TestState extends MusicalState {
 	private var _text:FlxText;
-
-	public function new()
-	{
-		super('assets/songs/Beat.ogg');
-	}
 	
     override public function create()
 	{
 		super.create();
+		
+		song.instrumental.load('assets/songs/Beat.ogg');
+		song.instrumental.looped = true;
+		song.bpm = 120;
+		song.timeSignature = new TimeSignature(4, 4);
+		song.play();
+
 		_text = new FlxText();
 		_text.size = 30;
 		add(_text);
 	}
-	
-	override public function beatHit(beat:Int):Void
+
+	override public function measureHit(measure:Int):Void
 	{
-		super.beatHit(beat);
-		trace(beat);
+		super.measureHit(measure);
+		trace('hello');
 	}
 
 	override public function stepHit(step:Int):Void
 	{
 		super.stepHit(step);
 		_text.text =
-			'curBeat: ${this.song.curBeat}\n' +
-			'curQuarter: ${this.song.curQuarter}\n' +
-			'curStep: ${this.song.curStep}';
+			'curMeasure: ${song.curMeasure}\n' +
+			'curBeat: ${song.curBeat}\n' +
+			'curQuarter: ${song.curQuarter}\n' +
+			'curStep: ${song.curStep}';
 	}
 }
