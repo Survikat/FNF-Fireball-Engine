61c8a16030e80d013291cfd059b1e2d10d3b8099
SILVERSTAR <silverstar.apollo@gmail.com>
2026-08-19 22:49:36 +0800

    add `Reactive<T>`

---
+++ source/engine/Reactive.hx
@@ -0,0 +1,34 @@
+package engine;
+
+import flixel.FlxG;
+
+typedef ReactiveArgs<T> = {
+    var initialValue:T;
+    var callback:T -> Void;
+}
+
+final class Reactive<T> {
+    public var value(get, set):T;
+
+    private var _value:T;
+    private var _callback:T -> Void;
+
+    public function new(args:ReactiveArgs<T>) {
+        if (args.callback == null) {
+            FlxG.log.error("Expected callback, got null");
+        }
+        _value = args.initialValue;
+        _callback = args.callback;
+    }
+
+    private function get_value():T {
+        return _value;
+    }
+
+    private function set_value(val:T):T {
+        if (val != _value) {
+            _callback(_value = val);
+        }
+        return _value;
+    }
+}
